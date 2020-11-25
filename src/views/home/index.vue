65711236afe0369a6604f7f91aeed6fa88f83ba1
Youwei_star <1176071733@qq.com>
2020-11-25 17:00:48 +0800

    开始首页开发

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,10 +1,43 @@
 <template>
-  <div class="home-container">首页</div>
+  <div class="home-container">
+    <van-nav-bar class="page-nav-bar">
+      <van-button
+        slot="title"
+        type="info"
+        size="small"
+        round
+        icon="search"
+        class="search-btn"
+        >搜索</van-button
+      >
+    </van-nav-bar>
+  </div>
 </template>
 <script>
 export default {
-  name: 'homeIndex'
+  name: 'homeIndex',
+  data() {
+    return {
+      value: ''
+    }
+  }
 }
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.home-container {
+  /deep/.van-nav-bar__title {
+    max-width: unset;
+  }
+  .van-icon-search::before {
+    color: #fff;
+  }
+  .search-btn {
+    width: 555px;
+    height: 64px;
+    background-color: #5babfb;
+    border: none;
+    font-size: 28px;
+  }
+}
+</style>
