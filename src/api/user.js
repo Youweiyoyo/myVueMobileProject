6afe9cb7c8fe40f0cbdaffdebcd5c38009506c30
Youwei_star <1176071733@qq.com>
2020-11-25 16:18:53 +0800

    请求拦截器设置成功

--- src/api/user.js
+++ src/api/user.js
@@ -1,6 +1,5 @@
 // 封装用户登录请求方法的模块
 import request from '@/utils/request.js'
-import store from '@/store'
 // 导出一个方法
 // data 接收一个参数，把接收到的参数传递到具体请求的位置
 export const login = data => {
@@ -21,10 +20,11 @@ export const sendSms = mobile => {
 export const getUserInfo = () => {
   return request({
     method: 'GET',
-    url: '/app/v1_0/user',
+    url: '/app/v1_0/user'
     // 发送请求头数据
-    headers: {
-      Authorization: `Bearer ${store.state.user.token}`
-    }
+    // headers: {
+    //   //  注意：该接口需要授权才能访问
+    //   Authorization: `Bearer ${store.state.user.token}`
+    // }
   })
 }
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,5 +1,23 @@
 import axios from 'axios'
+import store from '@/store'
 const request = axios.create({
   baseURL: 'http://ttapi.research.itcast.cn/'
 })
+// 请求拦截器
+request.interceptors.request.use(
+  // 请求发起会经过这里
+  function(config) {
+    const { user } = store.state
+    if (user && user.token) {
+      config.headers.Authorization = `Bearer ${user.token}`
+    }
+    // 这里务必要返回config配置对象
+    return config
+  },
+  function(error) {
+    // Do something with request error
+    return Promise.reject(error)
+  }
+)
+// 响应拦截器
 export default request
