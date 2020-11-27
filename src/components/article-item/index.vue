956453ea8364928f85727a337e0e1f5167291cc0
Youwei_star <1176071733@qq.com>
2020-11-27 17:03:10 +0800

    提交代码

--- src/components/article-item/index.vue
+++ src/components/article-item/index.vue
@@ -1,6 +1,8 @@
 <template>
   <van-cell class="article-item">
-    <div slot="title" class="title">{{ article.title }}</div>
+    <div slot="title" class="title van-multi-ellipsis--l2">
+      {{ article.title }}
+    </div>
     <div slot="label">
       <div class="cover-wrap" v-if="article.cover.type === 3">
         <div
@@ -8,13 +10,30 @@
           v-for="(img, index) in article.cover.images"
           :key="index"
         >
-          <van-image width="100" height="100" :src="img" />
+          <van-image
+            width="100"
+            height="100"
+            :src="img"
+            fit="cover"
+            class="cover-item-img"
+          />
         </div>
       </div>
-      <span>{{ article.aut_name }}</span>
-      <span>{{ article.comm_count }}评论</span>
-      <span>{{ article.pubdate }}</span>
+      <div class="label-info-wrap">
+        <span>{{ article.aut_name }}</span>
+        <span>{{ article.comm_count }}评论</span>
+        <span>{{ article.pubdate }}</span>
+      </div>
     </div>
+    <van-image
+      v-if="article.cover.type === 1"
+      slot="default"
+      class="right-cover"
+      fit="cover"
+      width="100"
+      height="100"
+      :src="article.cover.images[0]"
+    ></van-image>
   </van-cell>
 </template>
 
@@ -39,4 +58,41 @@ export default {
 }
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.article-item {
+  .title {
+    font-size: 32px;
+    color: #3a3a3a;
+  }
+  .van-cell__value {
+    flex: unset;
+    width: 232px;
+    height: 146px;
+    padding-left: 25px;
+  }
+  .right-cover {
+    width: 232px;
+    height: 146px;
+  }
+  .label-info-wrap span {
+    font-size: 22px;
+    color: #b4b4b4;
+    margin-right: 25px;
+  }
+  .cover-wrap {
+    display: flex;
+    padding: 30px 0;
+    .cover-item {
+      flex: 1;
+      height: 146px;
+      &:not(last-child) {
+        padding-right: 4px;
+      }
+      .cover-item-img {
+        width: 100%;
+        height: 146px;
+      }
+    }
+  }
+}
+</style>
